d9a277ba8ac9cf8a609c9d3336647c647c3bea93
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-25 15:06:42 +0200

    create `IsAdminOrIfAuthenticatedReadOnly` permission and set it up as api default

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -150,6 +150,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly"
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
