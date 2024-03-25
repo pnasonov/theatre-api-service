e0e0b53e3db9dcfc8de8fca514ea1fddad61069f
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-25 17:16:13 +0200

    set up urls and views for actor and genre models

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from theatre.models import (
+    Actor,
+    Genre,
+    Play,
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket,
+)
+from theatre.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    PlaySerializer,
+    TheatreHallSerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+)
+from theatre.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
--- theatre_api_service/urls.py
+++ theatre_api_service/urls.py
@@ -26,6 +26,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/doc/schema", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
