60e04cf90537faae4a78789f8074eae6dd543310
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-25 21:35:03 +0200

    change refresh and access jwt tokens time

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -167,7 +167,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
     "ROTATE_REFRESH_TOKENS": False,
 }
